2d309f992e3df45bbcc1ec859498cec92b041755
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-20 14:37:49 -0300

    adicionado código que seleciona dados do primeiro paciente a partir do document e o id da tag tr do paciente e que busca as tags td de peso e altura para selecionar os textos de peso e altura do paciente, além do início do código para cálculo imc do paciente

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,13 @@
-console.log("fui carregado de um arquivo externo");
-
 var titulo = document.querySelector(".titulo");
-
 titulo.textContent = "Aparecida Nutri";
+
+
+var paciente = document.querySelector("#primeiro-paciente");
+
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+var imc = peso / altura * altura;
