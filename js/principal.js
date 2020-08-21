bc89a81c9b7f1de1b485ac78282ac5ad94739b2e
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-08-21 15:52:26 -0300

    correção no cálculo de IMC, seleção da tag td que recebe o valor do IMC na tabela exibida por html e atribuição do valor do cálculo na tag td, atribuindo a ela o valor da variável imc

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,6 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutri";
 
-
 var paciente = document.querySelector("#primeiro-paciente");
 
 var tdPeso = paciente.querySelector(".info-peso");
@@ -10,4 +9,8 @@ var peso = tdPeso.textContent;
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var imc = peso / altura * altura;
+var tdImc = paciente.querySelector(".info-imc");
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc;
