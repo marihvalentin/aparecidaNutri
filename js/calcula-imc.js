f9008c6923b3524fcf6f7f92857f1995ad660aa1
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-11-04 23:04:09 -0300

    complemento de validação de peso e altura para cadastro de paciente

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -16,10 +16,10 @@ for (var i = 0; i < pacientes.length; i++)
 
   var tdImc = paciente.querySelector(".info-imc");
 
-  var pesoValido = true;
-  var alturaValida = true;
+  var pesoValido = validaPeso(peso); //true ou false
+  var alturaValida = validaAltura(altura); //true ou false
 
-  if(peso <= 0 || peso >= 1000)
+  if(!pesoValido)
   {
     console.log("Peso inválido!");
     pesoValido = false;
@@ -27,7 +27,7 @@ for (var i = 0; i < pacientes.length; i++)
     paciente.classList.add("paciente-invalido");
   }
 
-  if(altura <= 0 || altura >= 3.00)
+  if(!alturaValida)
   {
     console.log("Altura inválida!");
     alturaValida = false;
@@ -39,6 +39,24 @@ for (var i = 0; i < pacientes.length; i++)
   {
     var imc = calculaImc(peso, altura);
     tdImc.textContent = imc;
+  }
+}
+
+function validaPeso(peso) {
+  if (peso >= 0 && peso < 1000) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+function validaAltura(altura) {
+  if(altura >= 0 && altura <= 3.0) {
+    return true;
+  }
+  else {
+    return false;
   }
 }
 
--- js/form.js
+++ js/form.js
@@ -10,6 +10,11 @@ botaoAdicionar.addEventListener("click", function(event) {
   //cria a tr e a td do paciente
   var pacienteTr = montaTr(paciente);
 
+  if(!validaPaciente(paciente)) {
+    console.log("Paciente inválido");
+    return;
+  }
+
   //adicionando o paciente na tabela
   var tabela = document.querySelector("#tabela-pacientes");
   tabela.appendChild(pacienteTr);
@@ -52,4 +57,13 @@ function montaTd(dado, classe)
   td.classList.add(classe);
 
   return td;
+}
+
+function validaPaciente(paciente) {
+  if (validaPeso(paciente.peso)) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
