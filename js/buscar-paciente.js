6af1f8d0b259fd5b24cdcc8ff461c1e570cb5a52
Mariana Valentim Martins <mariana.martins@GEO.INTRANET>
2020-11-26 11:16:41 -0300

    adicionada implementação de tratamento de erro caso a busca não funcione por algum motivo

--- js/buscar-paciente.js
+++ js/buscar-paciente.js
@@ -1,21 +1,33 @@
 var botaoBuscar = document.querySelector("#buscar-pacientes");
 
-botaoBuscar.addEventListener("click", function() {
+botaoBuscar.addEventListener("click", function()
+{
   console.log("Buscando pacientes...");
 
   var xhr = new XMLHttpRequest();
 
   xhr.open("GET", "http://api-pacientes.herokuapp.com/pacientes");
 
-  xhr.addEventListener("load", function() {
-    var resposta = xhr.responseText;
+  xhr.addEventListener("load", function()
+  {
+    var erroAjax = document.querySelector("#erro-ajax");
+    if (xhr.status == 200)
+    {
+      erroAjax.classList.add("invisivel");
+      var resposta = xhr.responseText;
+      var pacientes = JSON.parse(resposta);
 
-    var pacientes = JSON.parse(resposta);
-
-    pacientes.forEach(function(paciente) {
-      adicionaPacienteNaTabela(paciente);
-    });
+      pacientes.forEach(function(paciente)
+      {
+        adicionaPacienteNaTabela(paciente);
+      });
+    }
+    else
+    {
+      console.log(xhr.status);
+      console.log(xhr.responseText);
+      erroAjax.classList.remove("invisivel");
+    }
   });
-
   xhr.send();
 });
